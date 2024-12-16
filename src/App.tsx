bfcc0a0968d915eb3bc570e0d4876c3133f7c7f2
Yiwei Liu <liu02yiwei@gmail.com>
2024-12-16 03:03:41 +0000

    feat: react flow setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
+import { ReactFlow, Controls, Background } from "@xyflow/react";
+import "@xyflow/react/dist/style.css";
+
+const nodes = [
+  {
+    id: "1",
+    position: { x: 0, y: 0 },
+    data: { label: "Hello" },
+  },
+  {
+    id: "2",
+    position: { x: 500, y: 500 },
+    data: { label: "World" },
+  },
+];
+
 function App() {
   return (
     <>
-      <main className="flex justify-center max-w-[1280px] mx-auto my-0 p-8 text-center">Main</main>
+      <main className="flex flex-col justify-center gap-y-8 max-w-[1280px] mx-auto my-0 p-8 text-center">
+        <p>Text</p>
+        <p>Tree</p>
+        <div className="w-full h-screen">
+          <ReactFlow nodes={nodes}>
+            <Background />
+            <Controls />
+          </ReactFlow>
+        </div>
+      </main>
     </>
   );
 }
